de16717a3b22a4ebdb7239e32feb23c3c4744ebd
Justen Walker <Justen.Walker@gmail.com>
2010-05-08 00:18:58 -0400

    Created userscript properties file

---
+++ userscript.props.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name           SomethingAwfuler
+// @version        #VERSION#
+// @author         Justen Walker (http://www.justenwalker.com)
+// @description    A collection of enhancements that aim to make the Something Awful forums more awful
+// @include        http://forums.somethingawful.com/*
+// @require	   http://userscripts.org/scripts/source/38788.user.js
+// @cfu:url        http://userscripts.org/scripts/source/65592.user.js
+// @cfu:meta       http://userscripts.org/scripts/source/65592.meta.js
+// @cfu:interval   1 day
+// @cfu:timestamp  uso:timestamp
+// @cfu:version    uso:version
+// @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// ==/UserScript==
