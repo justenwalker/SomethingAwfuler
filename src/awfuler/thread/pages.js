9fb5b151214bfecc6c646cf0576a2b624e32b12b
Justen Walker <justen.walker@gmail.com>
2010-05-14 11:10:44 -0400

    Added a skeleton loadPage method in awfuler.thread.pages

--- src/awfuler/thread/pages.js
+++ src/awfuler/thread/pages.js
@@ -1,6 +1,17 @@
 dojo.provide("awfuler.thread.pages");
+dojo.require("awfuler.util.meta");
+dojo.require("awfuler.thread.info");
+
 dojo.mixin(awfuler.thread,
 {pages: {
-//TODO: Need to implement functions here
+	loadPage: function(pagenumber,handler)
+	{
+		var href = awfuler.thread.info.getThreadLink(awfuler.thread.info.threadId,pagenumber);
+		dojo.xhrGet({
+			url: href,
+			handleAs: 'text',
+			load: handler
+		});
+	}
 }// pages
 }); // awfuler.thread
