b0dac31d172eeafdc99bf37612718780eb33deb8
Justen Walker <justen.walker@gmail.com>
2010-07-09 00:19:14 -0400

    Updated style to support awfulbar

--- SomethingAwfuler/src/awfuler/css/style.js
+++ SomethingAwfuler/src/awfuler/css/style.js
@@ -1,6 +1,5 @@
 dojo.provide('awfuler.css.style');
 dojo.addOnLoad(function() {
-	var s = awfuler.css.addCss;
 	var url = function(img) { 
 		var u = [];
 		u[0] = "url('";
@@ -8,51 +7,60 @@ dojo.addOnLoad(function() {
 		u[2] = "')";
 		return u.join('');
 	};
-s('navbar','div.bluebar_l', {
+	awfuler.css.addCss('awfulbar',[
+'.awfulbar', {
+	"width": "100%",
+	"height": '34px'
+},
+'.awfulbar_l', {
 	"position": 'fixed',
 	"bottom": '0px',
 	"left": '16px',
-	"width": '90%',
-});
-s('navbar','div.bluebar_l', {
+	"width": '100%'
+},
+'.awfulbar_l', {
 	"background-image": url('blue_bar_l.png'),
 	"background-repeat": 'no-repeat',
-	"height": '34px',
-});
-s('navbar','div.bluebar_r', {
+	"height": '34px'
+},
+'.awfulbar_r', {
 	"background-image": url('blue_bar_r.png'),
 	"background-repeat": 'no-repeat',
 	"height": '34px',
 	"width": '8px',
 	"float": 'right',
-	"margin-right": '-8px',
-});
-s('navbar','div.bluebar_bg', {
+	"margin-right": '-8px'
+},
+'.awfulbar_bg', {
 	"width": '100%',
 	"float": 'left',
 	"background-image": url('blue_bar_bg.png'),
 	"background-repeat": 'repeat-x',
 	"height": '34px',
 	"margin-left": '8px',
-	"margin-right": '8px',
-});
-s('navbar','div.bluebar_content', {
-	"border": '1px solid black',
-	"-webkit-border-radius": '2px',
-	"-moz-border-radius": '2px',
-	"border-radius": '2px',
+	"margin-right": '8px'
+},
+'.awfulbarTab', {
+	"color": "#fff",
+	"font-weight": "bold",
+	"border-left": '2px solid #006485',
 	"float": 'left',
 	"padding": '4px',
 	"margin-top": '2px',
-	"height": '26px',
-});
-s('navbar','div.bluebar_content:hover', {
+	"height": '26px'
+},
+'.awfulbarTab.Hover, .awfulbarTab.Active', {
+	"border": '2px solid #8F8F8F',
 	"margin-top": '-2px',
 	"margin-bottom": '2px',
 	"height": '32px',
 	"background-repeat": 'repeat-x',
 	"background-color": '#8F8F8F',
-	"background-image": url('gray_gradient_20px.gif'),
-});
+	"background-image": url('gray_gradient_20px.gif')
+},
+'.awfulbarTab.Active', {
+	"border": '2px solid #FF0000'
+}
+]);
 	awfuler.css.renderCss();
 });
