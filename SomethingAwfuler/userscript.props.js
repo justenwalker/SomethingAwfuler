ae0d7f2bd2b505a7603873577bd4d88efef62a57
Justen Walker <justen.walker@gmail.com>
2010-07-18 14:35:03 -0400

    Added a new updater

--- SomethingAwfuler/userscript.props.js
+++ SomethingAwfuler/userscript.props.js
@@ -4,11 +4,7 @@
 // @author         Justen Walker (http://www.justenwalker.com)
 // @description    A collection of enhancements that aim to make the Something Awful forums more awful
 // @include        http://forums.somethingawful.com/*
-// @require	   http://userscripts.org/scripts/source/38788.user.js
-// @cfu:url        http://userscripts.org/scripts/source/65592.user.js
-// @cfu:meta       http://userscripts.org/scripts/source/65592.meta.js
-// @cfu:interval   1 day
-// @cfu:timestamp  uso:timestamp
-// @cfu:version    uso:version
+// @require        http://userscripts.org/scripts/source/57756.user.js
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
+ScriptUpdater.check(#SCRIPTID#,#VERSION#)
