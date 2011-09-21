84eecb6557fc35358fc4d96f4ee7bd7ca0a14019
Justen Walker <justen.walker+github@gmail.com>
2011-09-20 20:20:31 -0400

    Released Script 1.3.1 with a minor change to the auto-updater

--- SomethingAwfuler/userscript.props.js
+++ SomethingAwfuler/userscript.props.js
@@ -4,7 +4,6 @@
 // @author         Justen Walker (http://www.justenwalker.com)
 // @description    A collection of enhancements that aim to make the Something Awful forums more awful
 // @include        http://forums.somethingawful.com/*
-// @require        http://userscripts.org/scripts/source/57756.user.js
+// @require        http://usocheckup.redirectme.net/#SCRIPTID#.js
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
-ScriptUpdater.check('#SCRIPTID#','#VERSION#');
